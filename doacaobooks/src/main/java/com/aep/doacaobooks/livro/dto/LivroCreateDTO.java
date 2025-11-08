41261005d3667ab004c0849c233b5b05f0eeb5d7
SeijiHoshino1 <seijihoshino@outlook.com>
2025-11-08 19:16:41 -0300

    feat(LivroCreateDTO): add livro create request DTO

---
+++ doacaobooks/src/main/java/com/aep/doacaobooks/livro/dto/LivroCreateDTO.java
@@ -0,0 +1,27 @@
+package com.aep.doacaobooks.livro.dto;
+
+import com.aep.doacaobooks.livro.entity.Enum.StatusLivro;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class LivroCreateDTO {
+
+    @NotNull
+    private String titulo;
+
+    @NotNull
+    private String autor;
+
+    @NotNull
+    private String curso;
+
+    @NotNull
+    private String disciplina;
+
+    @NotNull
+    private StatusLivro status;
+
+    @NotNull
+    private Long doadorId;
+}
