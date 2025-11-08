8e7b186e19f5509c92b414df409063ff772a58d9
SeijiHoshino1 <seijihoshino@outlook.com>
2025-11-08 19:17:06 -0300

    chore(LivroRepository): add find by titulo to use in DTO

--- doacaobooks/src/main/java/com/aep/doacaobooks/livro/repository/LivroRepository.java
+++ doacaobooks/src/main/java/com/aep/doacaobooks/livro/repository/LivroRepository.java
@@ -6,9 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.UUID;
+import java.util.Optional;
 
 @Repository
 public interface LivroRepository extends JpaRepository<Livro, Long> {
     List<Livro> findByStatus(StatusLivro livro);
+    Optional<Livro> findByTitulo(String titulo);
 }
