eade355954879ab3304857b5d5fffeb5cb92ac4b
SeijiHoshino1 <seijihoshino@outlook.com>
2025-11-08 19:19:21 -0300

    chore(UsuarioController): add get endpoint

--- doacaobooks/src/main/java/com/aep/doacaobooks/usuario/controller/UsuarioController.java
+++ doacaobooks/src/main/java/com/aep/doacaobooks/usuario/controller/UsuarioController.java
@@ -6,17 +6,22 @@ import com.aep.doacaobooks.usuario.service.UsuarioService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
+@ControllerAdvice
 @RequestMapping("/usuario")
 @RestController
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> get() {
+        return ResponseEntity.ok(usuarioService.findAll());
+    }
 
     @PostMapping("/cadastro")
     public ResponseEntity<Usuario> create(@Valid @RequestBody Usuario usuario) {
