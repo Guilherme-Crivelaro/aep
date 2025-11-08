7264d4fa500d1fec749df52d5df73d303bcdf4b5
SeijiHoshino1 <seijihoshino@outlook.com>
2025-11-08 19:17:39 -0300

    chore(LivroService): change method 'cadastrarLivro' parameters to DTO

--- doacaobooks/src/main/java/com/aep/doacaobooks/livro/service/LivroService.java
+++ doacaobooks/src/main/java/com/aep/doacaobooks/livro/service/LivroService.java
@@ -1,22 +1,36 @@
 package com.aep.doacaobooks.livro.service;
 
+import com.aep.doacaobooks.livro.dto.LivroCreateDTO;
 import com.aep.doacaobooks.livro.entity.Enum.StatusLivro;
 import com.aep.doacaobooks.livro.entity.Livro;
 import com.aep.doacaobooks.livro.exception.BookNotFoundException;
 import com.aep.doacaobooks.livro.repository.LivroRepository;
+import com.aep.doacaobooks.usuario.entity.Usuario;
+import com.aep.doacaobooks.usuario.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class LivroService {
 
     private final LivroRepository livroRepository;
+    private final UsuarioRepository usuarioRepository;
 
-    public Livro cadastrarLivro(Livro livro){
+    public Livro cadastrarLivro(LivroCreateDTO livroDTO){
+        Usuario doador = usuarioRepository.findById(livroDTO.getDoadorId())
+                .orElseThrow(() -> new RuntimeException("Doador não encontrado"));
+
+        Livro livro = new Livro();
+        livro.setTitulo(livroDTO.getTitulo());
+        livro.setAutor(livroDTO.getAutor());
+        livro.setCurso(livroDTO.getCurso());
+        livro.setDisciplina(livroDTO.getDisciplina());
+        livro.setStatus(livroDTO.getStatus());
+        livro.setDoador(doador);
+
         return livroRepository.save(livro);
     }
 
