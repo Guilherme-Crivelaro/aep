863701c66ebcba7d2eb7e09017084e3bcf847af7
SeijiHoshino1 <seijihoshino@outlook.com>
2025-11-09 01:13:01 -0300

    chore(DoacaoService): change return type to response DTOs and improve exceptions

--- doacaobooks/src/main/java/com/aep/doacaobooks/doacao/service/DoacaoService.java
+++ doacaobooks/src/main/java/com/aep/doacaobooks/doacao/service/DoacaoService.java
@@ -1,18 +1,21 @@
 package com.aep.doacaobooks.doacao.service;
 
 import com.aep.doacaobooks.doacao.dto.DoacaoRequestDTO;
+import com.aep.doacaobooks.doacao.dto.DoacaoResponseDTO;
 import com.aep.doacaobooks.doacao.entity.Doacao;
 import com.aep.doacaobooks.doacao.entity.Enum.StatusDoacao;
-import com.aep.doacaobooks.doacao.excpetion.IllegalStateException;
+import com.aep.doacaobooks.doacao.exception.DonationNotFoundException;
+import com.aep.doacaobooks.doacao.exception.DonationStatusException;
 import com.aep.doacaobooks.doacao.repository.DoacaoRepository;
 import com.aep.doacaobooks.livro.entity.Enum.StatusLivro;
 import com.aep.doacaobooks.livro.entity.Livro;
 import com.aep.doacaobooks.livro.exception.BookNotFoundException;
 import com.aep.doacaobooks.livro.repository.LivroRepository;
 import com.aep.doacaobooks.usuario.entity.Usuario;
-import com.aep.doacaobooks.usuario.exception.ConflictException;
+import com.aep.doacaobooks.usuario.exception.UserNotFoundException;
 import com.aep.doacaobooks.usuario.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -24,17 +27,18 @@ public class DoacaoService {
     private final DoacaoRepository doacaoRepository;
     private final LivroRepository livroRepository;
     private final UsuarioRepository usuarioRepository;
+    private final ModelMapper modelMapper;
 
-    public Doacao doarLivro(DoacaoRequestDTO dto){
+    public DoacaoResponseDTO doarLivro(DoacaoRequestDTO dto){
 
         Livro livro = livroRepository.findByTitulo(dto.getTituloLivro())
-                .orElseThrow(() -> new BookNotFoundException(dto.getTituloLivro()));
+                .orElseThrow(() -> new BookNotFoundException("Livro: " + dto.getTituloLivro()));
 
         Usuario usuario = usuarioRepository.findByEmail(dto.getEmailBeneficiario())
-                .orElseThrow(() -> new ConflictException("Usuario não encontrado"));
+                .orElseThrow(() -> new UserNotFoundException("email: " + dto.getEmailBeneficiario()));
 
         if(livro.getStatus()!= StatusLivro.DISPONIVEL){
-            throw new IllegalStateException("Livro não disponivel");
+            throw new DonationStatusException(dto.getTituloLivro());
         }
         livro.setStatus(StatusLivro.DOADO);
         livroRepository.save(livro);
@@ -45,16 +49,21 @@ public class DoacaoService {
         doacao.setDataAgendamento(LocalDate.now());
         doacao.setStatusDoacao(StatusDoacao.AGENDADA);
 
-        return doacaoRepository.save(doacao);
+        Doacao savedDoacao = doacaoRepository.save(doacao);
+
+        return modelMapper.map(savedDoacao, DoacaoResponseDTO.class);
     }
 
-    public Doacao pegarLivro(Long doacaoId, Usuario usuario) {
+    public DoacaoResponseDTO pegarLivro(Long doacaoId, Long usuarioId) {
         Doacao doacao = doacaoRepository.findById(doacaoId)
-                .orElseThrow(() -> new RuntimeException("Doação não encontrada"));
+                .orElseThrow(() -> new DonationNotFoundException("doacaoId: " + doacaoId));
 
         if (doacao.getStatusDoacao() != StatusDoacao.DISPONIVEL) {
-            throw new RuntimeException("Esse livro já foi doado");
+            throw new DonationStatusException("doacaoId: " + doacaoId);
         }
+
+        Usuario usuario = usuarioRepository.findById(usuarioId)
+                .orElseThrow(() -> new UserNotFoundException("usuarioId: " + usuarioId));
 
         doacao.setStatusDoacao(StatusDoacao.CONCLUIDA);
         doacao.setBeneficiario(usuario);
@@ -63,12 +72,14 @@ public class DoacaoService {
         livro.setStatus(StatusLivro.DOADO);
         livroRepository.save(livro);
 
-        return doacaoRepository.save(doacao);
+        Doacao savedDoacao = doacaoRepository.save(doacao);
+
+        return modelMapper.map(savedDoacao, DoacaoResponseDTO.class);
     }
 
     public Doacao atualizarStatus(Long id, StatusDoacao novoStatus){
         Doacao doacao = doacaoRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Doação não encontrada"));
+                .orElseThrow(() -> new DonationStatusException("doacaoId: " + id));
 
         doacao.setStatusDoacao(novoStatus);
         return doacaoRepository.save(doacao);
