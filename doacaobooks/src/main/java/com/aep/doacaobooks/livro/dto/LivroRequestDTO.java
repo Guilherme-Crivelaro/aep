9a962503bac9537ec3b5db9c1f102e80e4607593
SeijiHoshino1 <seijihoshino@outlook.com>
2025-11-09 01:14:01 -0300

    fix(LivroCreateDTO): fix class name

--- doacaobooks/src/main/java/com/aep/doacaobooks/livro/dto/LivroRequestDTO.java
+++ doacaobooks/src/main/java/com/aep/doacaobooks/livro/dto/LivroRequestDTO.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
-public class LivroCreateDTO {
+public class LivroRequestDTO {
 
     @NotNull
     private String titulo;
